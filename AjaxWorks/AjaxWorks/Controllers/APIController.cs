8360ec8caa9ea04acf83d767995d2de747464d26
chingspring <149592774+chingspring@users.noreply.github.com>
2024-01-29 19:14:18 +0800

    ajax

---
+++ AjaxWorks/AjaxWorks/Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using AjaxWorks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxWorks.Controllers
+{
+    public class APIController : Controller
+    {
+            private readonly MyDBContext _context;
+            public APIController(MyDBContext context)
+            {
+                _context = context;
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult Cities()
+            {
+                var cities = _context.Addresses.Select(a => a.City).Distinct();
+                return Json(cities);
+            }
+
+        
+    }
+}
